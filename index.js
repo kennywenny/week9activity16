466177826359228407a658b35572f8138177347b
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-17 20:17:09 +0000

    With switch statement

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ const maths = require('./maths.js')
 const operation = process.argv[2]
 const firstNumber = parseInt(process.argv[3])
 const secondNumber = parseInt(process.argv[4])
-const result = maths[operation](firstNumber, secondNumber)
-console.log(result)
+switch (operation) {
+  case 'sum':
+    console.log(maths.sum(firstNumber, secondNumber))
+    return
+  case 'difference':
+    console.log(maths.difference(firstNumber, secondNumber))
+    return
+  case 'product':
+    console.log(maths.product(firstNumber, secondNumber))
+    return
+  case 'quotient':
+    console.log(maths.quotient(firstNumber, secondNumber))
+    return
+}
